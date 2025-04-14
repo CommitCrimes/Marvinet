6caeb70ba27f52b0bbe2b2bcfb97b69621905aac
Arnaud Méron <arnaud.meron2004@gmail.com>
2025-04-14 12:15:05 +0200

    added somthing that could work idk

--- jenkins-dsl/job_dsl.groovy
+++ jenkins-dsl/job_dsl.groovy
@@ -1,14 +1,12 @@
-// Fichier job_dsl.groovy
-
-// 1. Dossier "Tools"
+// 1. Folder Tools
 folder('Tools') {
     description('Folder for miscellaneous tools.')
 }
 
-// 2. Job "clone-repository"
+// 2. Job: clone-repository
 freeStyleJob('Tools/clone-repository') {
     description('Clone a Git repository using the provided URL.')
-    
+
     parameters {
         stringParam('GIT_REPOSITORY_URL', '', 'Git URL of the repository to clone')
     }
@@ -22,9 +20,9 @@ freeStyleJob('Tools/clone-repository') {
     }
 }
 
-// 3. Job "SEED"
+// 3. Job: SEED
 freeStyleJob('Tools/SEED') {
-    description('Seed job to generate a new job from GitHub.')
+    description('Seed job to generate jobs from GitHub repos.')
 
     parameters {
         stringParam('GITHUB_NAME', '', 'GitHub repository owner/repo_name (e.g.: EpitechIT31000/chocolatine)')
@@ -37,34 +35,8 @@ freeStyleJob('Tools/SEED') {
 
     steps {
         dsl {
-            text("""
-freeStyleJob("\${DISPLAY_NAME}") {
-    description("Job generated from GitHub repository \${GITHUB_NAME}")
-    
-    properties {
-        githubProjectUrl("https://github.com/\${GITHUB_NAME}")
-    }
-
-    scm {
-        git("https://github.com/\${GITHUB_NAME}.git")
-    }
-
-    triggers {
-        scm('H/1 * * * *')
-    }
-
-    wrappers {
-        preBuildCleanup()
-    }
-
-    steps {
-        shell('make fclean')
-        shell('make')
-        shell('make tests_run')
-        shell('make clean')
-    }
-}
-            """)
+            external('/var/jenkins_home/dsl/template_job.groovy')
+            removeAction('IGNORE')
         }
     }
 }
